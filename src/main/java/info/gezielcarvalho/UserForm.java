e1bd8606f239bef76f0cd42ff36bce6edd0e0607
Geziel Carvalho <geziel@consultarer.com>
2025-05-17 22:26:19 +0000

    refactor UserForm bean and add entity

--- src/main/java/info/gezielcarvalho/UserForm.java
+++ src/main/java/info/gezielcarvalho/UserForm.java
@@ -2,33 +2,56 @@ package info.gezielcarvalho;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import javax.persistence.*; // For EntityManager, etc.
+import info.gezielcarvalho.entities.UserFormEntity; // Assuming this is the correct package for UserFormEntity
 
 @ManagedBean
-@RequestScoped
-public class UserForm {
+@SessionScoped
+public class UserForm implements Serializable {
+
     private String name;
-    private boolean agreed;
+    private boolean termsAccepted;
+
+    public String submit() {
+        if (name != null && !name.isEmpty() && termsAccepted) {
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("HelloPU");
+            EntityManager em = emf.createEntityManager();
+            EntityTransaction tx = em.getTransaction();
+
+            try {
+                tx.begin();
+                UserFormEntity entity = new UserFormEntity();
+                entity.setName(name);
+                entity.setTermsAccepted(termsAccepted);
+                em.persist(entity);
+                tx.commit();
+            } catch (Exception e) {
+                if (tx.isActive()) tx.rollback();
+                e.printStackTrace();
+            } finally {
+                em.close();
+                emf.close();
+            }
+
+            return "success.xhtml";
+        }
+        return "index.xhtml";
+    }
 
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
-    public boolean isAgreed() {
-        return agreed;
+    public boolean isTermsAccepted() {
+        return termsAccepted;
     }
-
-    public void setAgreed(boolean agreed) {
-        this.agreed = agreed;
-    }
-
-    public String submit() {
-        if (name != null && !name.trim().isEmpty() && agreed) {
-            return "success"; // success.xhtml
-        }
-        return "failure"; // failure.xhtml
+    public void setTermsAccepted(boolean termsAccepted) {
+        this.termsAccepted = termsAccepted;
     }
 }
+
---
+++ src/main/java/info/gezielcarvalho/entities/UserFormEntity.java
@@ -0,0 +1,126 @@
+package info.gezielcarvalho.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "user_form")
+public class UserFormEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    @Column(name = "terms_accepted")
+    private boolean termsAccepted;
+
+    @Column(name = "created_at", updatable = false, insertable = false)
+    private java.sql.Timestamp createdAt;
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public boolean isTermsAccepted() {
+        return termsAccepted;
+    }
+    public void setTermsAccepted(boolean termsAccepted) {
+        this.termsAccepted = termsAccepted;
+    }
+    public java.sql.Timestamp getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(java.sql.Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+    @Override
+    public String toString() {
+        return "UserFormEntity{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", termsAccepted=" + termsAccepted +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UserFormEntity)) return false;
+
+        UserFormEntity that = (UserFormEntity) o;
+
+        if (termsAccepted != that.termsAccepted) return false;
+        if (!id.equals(that.id)) return false;
+        if (!name.equals(that.name)) return false;
+        return createdAt.equals(that.createdAt);
+    }
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + name.hashCode();
+        result = 31 * result + (termsAccepted ? 1 : 0);
+        result = 31 * result + createdAt.hashCode();
+        return result;
+    }
+    public UserFormEntity() {
+    }
+    public UserFormEntity(String name, boolean termsAccepted) {
+        this.name = name;
+        this.termsAccepted = termsAccepted;
+    }
+    public UserFormEntity(Integer id, String name, boolean termsAccepted) {
+        this.id = id;
+        this.name = name;
+        this.termsAccepted = termsAccepted;
+    }
+    public UserFormEntity(Integer id, String name, boolean termsAccepted, java.sql.Timestamp createdAt) {
+        this.id = id;
+        this.name = name;
+        this.termsAccepted = termsAccepted;
+        this.createdAt = createdAt;
+    }
+    public UserFormEntity(String name, boolean termsAccepted, java.sql.Timestamp createdAt) {
+        this.name = name;
+        this.termsAccepted = termsAccepted;
+        this.createdAt = createdAt;
+    }
+    public UserFormEntity(Integer id) {
+        this.id = id;
+    }
+    public UserFormEntity(String name) {
+        this.name = name;
+    }
+    public UserFormEntity(boolean termsAccepted) {
+        this.termsAccepted = termsAccepted;
+    }
+    public UserFormEntity(java.sql.Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+    public UserFormEntity(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    public UserFormEntity(Integer id, boolean termsAccepted) {
+        this.id = id;
+        this.termsAccepted = termsAccepted;
+    }
+    public UserFormEntity(String name, java.sql.Timestamp createdAt) {
+        this.name = name;
+        this.createdAt = createdAt;
+    }
+    public UserFormEntity(boolean termsAccepted, java.sql.Timestamp createdAt) {
+        this.termsAccepted = termsAccepted;
+        this.createdAt = createdAt;
+    }
+
+}
